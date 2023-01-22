a1db2ec993e9533e31ee3697cbdf83c02407fda2
Tadjyk <122217618+Tadjyk@users.noreply.github.com>
2023-01-22 18:54:01 +0600

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+{ 
+     newPosition = transform.position;
+}
+// Update is called once per frame
+void Update()
+{
+    newPosition.z += speed * Time.deltaTime;
+    transform.position = newPosition;
+}
+}
